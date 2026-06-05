9317b6696b9b0af2550dc23ea2d09cefb9d47e05
spoorjith1 <spoorjith1@gmail.com>
2026-06-05 07:00:42 +0530

    user profile view & edit

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from .models import User
 from rest_framework import generics
-from .serializers import RegistrationSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer, UserProfileViewSerializer, UserProfileEditSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class UserRegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
-    serializer_class = RegistrationSerializer
+    serializer_class = UserRegistrationSerializer
     queryset = User.objects.all()
+
+
+class OwnProfileView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserProfileViewSerializer
+    queryset = User.objects.all()
+    
+    def get_object(self):
+        return self.request.user
+
+
+class OwnProfileEditView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserProfileEditSerializer
+    queryset = User.objects.all()
+    
+    def get_object(self):
+        return self.request.user
+    
+    def patch(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
--- backend-drf/shopMore_config/settings.py
+++ backend-drf/shopMore_config/settings.py
@@ -127,7 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
 
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
